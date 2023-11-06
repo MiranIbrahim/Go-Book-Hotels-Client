3271a87fc3b85983edcd9431cce0dabfb22dc9ed
Miran Ibrahim <moeenuddin.miran@gmail.com>
2023-11-07 02:57:36 +0600

    static review section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner";
 import NewsLetter from "./NewsLetter";
+import Reviews from "./Reviews";
 
 
 const Home = () => {
     return (
         <main className='container mx-auto'>
             <Banner></Banner>
+            <Reviews></Reviews>
             <NewsLetter></NewsLetter>
         </main>
     );
