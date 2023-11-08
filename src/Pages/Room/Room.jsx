b4d6b67d60106e586606eeba64e00ec8a33ab575
Miran Ibrahim <moeenuddin.miran@gmail.com>
2023-11-08 15:49:09 +0600

    sorting according to price done

--- src/Pages/Room/Room.jsx
+++ src/Pages/Room/Room.jsx
@@ -4,19 +4,24 @@ import RoomCard from "./RoomCard";
 
 const Room = () => {
   const [rooms, setRooms] = useState([]);
-  const [price, setPrice] = useState("");
-  console.log(price);
+  const [sortBy, setSortBy] = useState("asc");
+
   useEffect(() => {
-    axios.get("http://localhost:5000/rooms").then((data) => {
+    let url = "http://localhost:5000/rooms";
+
+  if (sortBy) {
+    url += `?sort=${sortBy}`;
+  }
+    axios.get(url).then((data) => {
       console.log(data.data);
       setRooms(data.data);
     });
-  }, []);
+  }, [sortBy]);
   return (
     <section className="flex flex-col items-center gap-5">
       <select
         onChange={(e) => {
-          setPrice(e.target.value);
+          setSortBy(e.target.value);
         }}
         className="select select-info  m-5"
       >
@@ -26,6 +31,7 @@ const Room = () => {
         <option value="asc">Low to High</option>
         <option value="desc">High to Low</option>
       </select>
+
       <div className="container mx-auto grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {rooms?.map((room) => (
           <RoomCard key={room.id} room={room}></RoomCard>
